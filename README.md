f7cfefbbfda3eafa6732e05c73cfc448c345540f
BMDACMER <1163753605@qq.com>
2020-02-19 10:23:44 +0800

    first code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### 学习java基础  课程代码来源于尚硅谷2019年培训视频
+
+课件及相关代码：<br/>
+链接：https://pan.baidu.com/s/1Gy-cN_s77DC9fP_y8EbWrQ <br/>
+提取码：0hdc
---
+++ day01/src/com/atguigu/java/HelloChina.java
@@ -0,0 +1,9 @@
+package com.atguigu.java;
+
+public class HelloChina {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java world");
+    }
+
+}
