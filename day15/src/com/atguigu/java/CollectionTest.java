176a72e6126645c53246c95c1d11bab32c19e9b7
BMDACMER <1163753605@qq.com>
2020-03-07 22:58:14 +0800

    对应day23  集合（非常重要）

---
+++ day15/src/com/atguigu/java/CollectionTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.java;
+
+import org.junit.Test;
+
+/**
+ * 向collection接口的实现类的对象中添加数据obj时，要求obj所在类要重写equals()
+ */
+public class CollectionTest {
+
+    @Test
+    public void test1(){
+
+    }
+}
